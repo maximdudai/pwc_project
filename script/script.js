ea612c47d755c04ff8bc03f143912c08f74d1020
maximdudai <maxim.dudai01@gmail.com>
2023-11-18 22:24:51 +0000

    section ids for navigation bar, adaptable navigation bar

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const carouselItem = document.querySelectorAll(".carousel-item");
+const carouselItem = document?.querySelectorAll(".carousel-item");
 
 carouselItem.forEach((item) => {
     item.addEventListener("click", (e) => {
         const carouselItemActive = document.querySelector(".carousel-item.active");
         
-        carouselItemActive.classList.remove("active");
-        item.classList.add("active");
+        carouselItemActive?.classList.remove("active");
+        item?.classList.add("active");
     });
 });
