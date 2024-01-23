154393790e5a16b51c15015a38e1e10cb25e25c4
maximdudai <maxim.dudai01@gmail.com>
2024-01-23 22:34:12 +0000

    fixes

--- script/animalData.js
+++ script/animalData.js
@@ -4,8 +4,8 @@ const animalCard = $('.animalCard');
 const API_LINK = "https://api.petfinder.com/v2/animals";
 const apiConfig = {
   API_LINK: "https://api.petfinder.com/v2/animals",
-  Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJmUURFVmZScGtsS3JteDZMOTZUN3EwcWpuYUxXQ0hZd0dyTmxXOWt4VlFRQUlnS1NKZiIsImp0aSI6ImYwMTk5OTBlNWNjMzA3ZjI4NjQyNzQ1MWRkZDgxZTA3OGJiZjk3ZWM1NmM2MDc2MDJlN2I3NGE4ZDNhMmZmMGQ2ZGM1NDk5YTQ3Mzc4ZTg5IiwiaWF0IjoxNzA1OTUyMzUzLCJuYmYiOjE3MDU5NTIzNTMsImV4cCI6MTcwNTk1NTk1Mywic3ViIjoiIiwic2NvcGVzIjpbXX0.q3gGUx3xET-S_Dm9gou65p-wuQt-PBJAX6ycVszEekw-QQZ9ZSoStHoum6QVTGOFh5PMro17E6IE0HYRVu5tu6uESbB9OE0dI2fV5SCueey28wDZOwCwan8bvRqvHUE8AfsldRSDODruA9pjrkyyBEIUakra4jsT8bIeRFYZj0NBu5emBEEPFccET4ebEv0044Zafk62alVqeatlvhpPLHcQKz6qW6qS4pkyB6pzM9Ct98_KFWXp9NkyQCb76-EmwBKi4vAftsDUAGVS46la3FX-kNLH7x1votu2GXjMMZ4ICyHXQoUV6KVqt8vxULSiPdTqb2CaQWmWsYYl9U5Maw",
-  limitData: 10
+  Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJmUURFVmZScGtsS3JteDZMOTZUN3EwcWpuYUxXQ0hZd0dyTmxXOWt4VlFRQUlnS1NKZiIsImp0aSI6ImQzNGIyOWQ2NDhlMDY4ZjM3MzdkNjZkNjJkYjM4NzY4ZTRjMzI1YjVmZWY0YTBlYjZkYjA4ZjAxOTRhYTg0N2Y5NTQ2NWQwN2RlNTcwNTU0IiwiaWF0IjoxNzA2MDQ0OTMwLCJuYmYiOjE3MDYwNDQ5MzAsImV4cCI6MTcwNjA0ODUzMCwic3ViIjoiIiwic2NvcGVzIjpbXX0.mG8h-_16fiTArbjV2qg_PAQig50zAOpncgDp8tQXejIvSwUzumNOCpHCW1RWMHy-y4rYjevqO7ywGUMkzAcaqK_62guyV1no0h8V0S1j9qYoYr4begLx-cFOZi0_PTvBsfik4wC55a6Cizf5_N1sC2ZcNh2pIxvE2-Yk93BfOq5hGd4IfqE3fiu5MhbH8BeCg2EDwDyXtRCkBaoAPCHZ4on8llM4pgR3a9r25TSfRebVhRjpK21Wo4OWr5_ww5JMIXFpeWQmq5z9qFoPJk30Dajv-W6FEr8y4hx_Im-R_GIlB4btzoi4oHkoVrVZgo_dDeXF1IZUm_VtPKmrCgXPIA",
+  limitData: 9
 }
 
 const createAnimalCard = (index, photos, name, age) => {
@@ -61,33 +61,57 @@ $(document).ready(() => {
 
 });
 
+const getAttributeName = (key) => {
+  const name = {
+    'spayed_neutered': 'Esterilizado(a)',
+    'house_trained': 'Treinado(a) para ir ao banheiro',
+    'declawed': 'Descornada',
+    'special_needs': 'Necessidades especiais',
+    'shots_current': 'Vacinas em dia'
+  };
+
+  return name[key] || key;
+}
+
+
 const showAnimalData = (data) => {
-  const modal = $('.modal-body');
+  const modal = $('.card-text');
 
   // modal.find('.detalhesSobreAnimal').empty();
+
+  console.log(data);
 
   const name = data?.name || "Sem nome";
   const age = data?.age || "Sem idade";
 
-  const attributos = data?.attributes || [];
 
   const description = data?.description || "Sem descrição";
   const gender = data?.gender || "Sem gênero";
   const size = data?.size || "Sem tamanho";
   const status = data?.status || "Sem status";
   const url = data?.url || "Sem url";
+  const contact = data?.contact[0] || "Sem dados"
+
+  const attributos = data?.attributes || [];
+  const attributosLista = $('.detalhesSobreAnimal').find('.animalAttributos');
+  
+  console.log('Attributes:', attributos);
+
+  $.each(attributos, function (key, value) {
+    const listItem = $('<li></li>');
+    listItem.html(`${getAttributeName(key)}: ${value === null ? 'N/A' : value ? 'Sim' : 'Não'}`);
+    listItem.appendTo(attributosLista);
+  });
+  
 
 
-  const attributosLista = modal.find('.animalAttributos');
-
-  if (attributos.length > 0) {
-    $.each(attributos, function (key, value) {
-      const listItem = $('<li></li>');
-      listItem.text(`${key}: ${value === null ? 'N/A' : !value ? 'Não' : 'Sim'}`);
-      attributosLista.append(listItem);
-    });
-  }
-
+  // animalContact
+  // animalDescription
+  // animalGender
+  // animalData
+  // animalSize
+  // animalType
+  // animalStatus
 
   modal.find('.animalName').text(name);
   modal.find('.animalAge').text(age);
@@ -95,6 +119,7 @@ const showAnimalData = (data) => {
   modal.find('.animalGender').text(gender);
   modal.find('.animalSize').text(size);
   modal.find('.animalStatus').text(status);
+  modal.find('.animalContact').text(contact);
 
   modal.find('.animalLink').text('LINK');
   modal.find('.animalLink').attr('href', url);
